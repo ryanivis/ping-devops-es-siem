5a72d58a8db6b956fb775f4db163ca83e1424836
Ryan Ivis <ryanivis@ivisr-ryan-r.local>
2020-02-16 11:48:50 -0700

    fully baked ES stack soaking PF Audit logs! -- Auto config loader for kibana and ES as well.

---
+++ elasticsearch-siem/ping_siem_config_bootstrap/es_configurationloader.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Simple script to load ping fed audit template and kibana saved ndjson
+
+es_status="red"
+
+while [ "$es_status" != "green" ]
+do
+  echo "Status Not Green Yet"
+  sleep 3
+  health=$(curl -u elastic:2FederateM0re --insecure https://es01:9200/_cluster/health)
+  es_status=$(expr "$health" : '.*"status":"\([^"]*\)"')
+done
+
+sleep 120
+echo "loading templates"
+curl -X PUT "https://es01:9200/_template/pf_audit?pretty" --insecure -u elastic:2FederateM0re -H 'Content-Type: application/json' -d"@/usr/share/elasticsearch/config/es_config/templates.json"
+echo "loading kibana saved objects"
+curl -X POST "https://kibana:5601/api/saved_objects/_import" --insecure -u elastic:2FederateM0re -H "kbn-xsrf: true" --form file="@/usr/share/elasticsearch/config/kibana_config/kib_base.ndjson"
+
+
+
+
+

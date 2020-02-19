3a68a25964d0feb51d9135b8c0edc0232031f359
ryanivis <ryanivis@pingidentity.com>
2020-02-18 21:41:04 -0700

    several updates including env vars for passwords, and splitting out the enrichment service

--- elasticsearch-siem/ping_siem_config_bootstrap/es_configurationloader.sh
+++ elasticsearch-siem/ping_siem_config_bootstrap/es_configurationloader.sh
@@ -1,29 +1,10 @@
 #!/bin/bash
-#Simple script to load ping fed audit template and kibana saved ndjson.
-
-es_status="red"
-
-while [ "$es_status" != "green" ]
-do
-  echo "Status Not Green Yet"
-  sleep 3
-  health=$(curl -u elastic:2FederateM0re --insecure https://es01:9200/_cluster/health)
-  es_status=$(expr "$health" : '.*"status":"\([^"]*\)"')
-done
-
-sleep 120
-echo "loading templates"
-curl -X PUT "https://es01:9200/_template/pf_audit?pretty" --insecure -u elastic:2FederateM0re -H 'Content-Type: application/json' -d"@/usr/share/elasticsearch/config/es_config/templates.json"
-echo "loading kibana saved objects"
-curl -X POST "https://kibana:5601/api/saved_objects/_import" --insecure -u elastic:2FederateM0re -H "kbn-xsrf: true" --form file="@/usr/share/elasticsearch/config/kibana_config/kib_base.ndjson"
-
-echo "starting and running enrichment process"
-
-
 #
 # !!!! ENDLESS LOOP
 # THIS WILL PULL THREAT INTEL EVERY 2000 SECONDS AND PLACE IN THE ENRICHMENT CACHE FOLDER
 #
+
+echo "starting and running enrichment process"
 
 while [ 1 -eq 1 ]
 do
@@ -31,6 +12,3 @@ do
   python /usr/share/elasticsearch/config/bootstrap/enrichment.py
   sleep 2000
 done
-
-
-

25bfc057fa07b4a30887aea48ef76992ceeb0d7b
ryanivis <ryanivis@pingidentity.com>
2020-03-02 08:02:59 -0700

    modifications to saved objects and password's in config file

--- elasticsearch-siem/ping_siem_config_bootstrap/saved_state_loader.sh
+++ elasticsearch-siem/ping_siem_config_bootstrap/saved_state_loader.sh
@@ -11,9 +11,11 @@ do
   es_status=$(expr "$health" : '.*"status":"\([^"]*\)"')
 done
 
-sleep 120
+sleep 30 # Allow Some time For ElasticSearch Cluster to stabilize 
 echo "loading templates"
-curl -X PUT "https://es01:9200/_template/pf_audit?pretty" --insecure -u elastic:2FederateM0re -H 'Content-Type: application/json' -d"@/usr/share/elasticsearch/config/es_config/templates.json"
+curl -X PUT "https://es01:9200/_template/pf_audit?pretty" --insecure -u elastic:$ELASTIC_PASSWORD -H 'Content-Type: application/json' -d"@/usr/share/elasticsearch/config/es_config/templates.json"
+
+
 echo "loading kibana saved objects"
-curl -X POST "https://kibana:5601/api/saved_objects/_import" --insecure -u elastic:2FederateM0re -H "kbn-xsrf: true" --form file="@/usr/share/elasticsearch/config/kibana_config/kib_base.ndjson"
+curl -X POST "https://kibana:5601/api/saved_objects/_import" --insecure -u elastic:$ELASTIC_PASSWORD -H "kbn-xsrf: true" --form file="@/usr/share/elasticsearch/config/kibana_config/kib_base.ndjson"
 
